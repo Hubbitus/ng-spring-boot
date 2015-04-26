cc1a384a7b6048f4eb9549e597cb921fe7c78170
Hubbitus <pahan@hubbitus.info>
2015-04-26 23:13:58 +0300

    Try make WAR to run directly and under Tomcat

--- src/main/java/be/g00glen00b/Application.java
+++ src/main/java/be/g00glen00b/Application.java
@@ -3,11 +3,24 @@ package be.g00glen00b;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * http://stackoverflow.com/questions/27904594/spring-boot-war-deployed-to-tomcat
+ * http://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#howto-create-a-deployable-war-file
+ **/
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
